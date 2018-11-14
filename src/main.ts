cce88c7de87e62a8b16d1b6f82ae81756e21127b
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2018-11-14 22:40:34 +0900

    rename applicationts to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,39 @@
 import {app} from 'electron'
-import { Application } from './application'
+import { Menu } from 'electron';
+import  * as path from 'path';
+import { MainWindow } from './mainWindow'
 
-let application = new Application(app);
+export class Application {
+
+    private mainWindow: MainWindow | null = null;
+
+    constructor(public app: Electron.App) {
+        this.app.on('ready', () => this.onReady());
+        this.app.on('window-all-closed', () => this.onWindowAllClosed());
+    }
+
+    private onWindowAllClosed() {
+        if (process.platform != 'darwin') {
+            this.app.quit();
+        }
+    }
+
+    private onReady() {
+        this.mainWindow = new MainWindow(path.join(__dirname, '../index.html'));
+
+        var menu = Menu.buildFromTemplate([{
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Exit',
+                    accelerator: 'CmdOrCtrl+Q',
+                    click: () => { this.app.quit(); }
+                }]
+        }
+        ]);
+
+        Menu.setApplicationMenu(menu);
+    }
+}
+
+new Application(app);
--- src/mainWindow.ts
+++ src/mainWindow.ts
@@ -1,15 +1,14 @@
 import { BrowserWindow } from 'electron';
-import { dialog, ipcMain, webContents } from 'electron';
 
 export class MainWindow {
 
-    private window: Electron.BrowserWindow = null;
+    private window: Electron.BrowserWindow | null = null;
     
     constructor(url: string) {
         this.window = new BrowserWindow({width: 800, height: 600});
         this.window.loadFile(url);
 
-        this.window.on('closed', this.onClosed);
+        this.window.on('closed', () => this.onClosed());
     }
 
     private onClosed() {
