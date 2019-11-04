e41f5471aa50c1269897e4dc4431e85f5504ea74
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2019-11-04 22:02:11 +0900

    Update tsconfig.json

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,39 +1,48 @@
-import {app} from 'electron'
+import { app } from 'electron'
+import { BrowserWindow } from 'electron';
 import { Menu } from 'electron';
-import  * as path from 'path';
-import { MainWindow } from './mainWindow'
+import * as path from 'path';
 
 export class Application {
 
-    private mainWindow: MainWindow | null = null;
+  private mainWindow: Electron.BrowserWindow | null = null;
 
-    constructor(public app: Electron.App) {
-        this.app.on('ready', () => this.onReady());
-        this.app.on('window-all-closed', () => this.onWindowAllClosed());
+  constructor(public app: Electron.App) {
+    this.app.on('ready', () => this.onReady());
+    this.app.on('window-all-closed', () => this.onWindowAllClosed());
+  }
+
+  private onWindowAllClosed() {
+    if (process.platform != 'darwin') {
+      this.app.quit();
     }
+  }
 
-    private onWindowAllClosed() {
-        if (process.platform != 'darwin') {
-            this.app.quit();
-        }
+  private onReady() {
+    this.mainWindow = new BrowserWindow({
+      width: 800,
+      height: 400
+    });
+    this.mainWindow.loadFile(path.join(__dirname, '../index.html'));
+    this.mainWindow.on('closed', () => this.onClosed());
+
+    const menu = Menu.buildFromTemplate([{
+      label: '&File',
+      submenu: [
+        {
+          label: 'E&xit',
+          accelerator: 'CmdOrCtrl+Q',
+          click: () => { this.app.quit(); }
+        }]
     }
+    ]);
 
-    private onReady() {
-        this.mainWindow = new MainWindow(path.join(__dirname, '../index.html'));
+    Menu.setApplicationMenu(menu);
+  }
 
-        var menu = Menu.buildFromTemplate([{
-            label: '&File',
-            submenu: [
-                {
-                    label: 'E&xit',
-                    accelerator: 'CmdOrCtrl+Q',
-                    click: () => { this.app.quit(); }
-                }]
-        }
-        ]);
-
-        Menu.setApplicationMenu(menu);
-    }
+  private onClosed() {
+    this.mainWindow = null;
+  }
 }
 
 new Application(app);
