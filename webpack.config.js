278adf562585b3488f320f2d6a2ab093c883322d
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2019-11-06 20:41:07 +0900

    Delete renderer files

--- webpack.config.js
+++ webpack.config.js
@@ -30,31 +30,6 @@ var main = {
   },
 };
 
-var renderer = {
-  mode: 'development',
-  target: 'electron-renderer',
-  entry: path.join(__dirname, 'src', 'renderer', 'index'),
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist', 'scripts')
-  },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx']
-  },
-  module: {
-    rules: [{
-      test: /\.(tsx|ts)$/,
-      use: [
-        'ts-loader'
-      ],
-      include: [
-        path.resolve(__dirname, 'src'),
-        path.resolve(__dirname, 'node_modules'),
-      ],
-    }]
-  },
-};
-
 module.exports = [
-  main, renderer
+  main,
 ];
