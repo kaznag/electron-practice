d640b3a30dcb820639b0625afa30a19c6c53b2de
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2019-11-08 21:34:22 +0900

    Inhibit multiple instance

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,8 +8,13 @@ export class Application {
   private mainWindow: Electron.BrowserWindow | null = null;
 
   constructor(public app: Electron.App) {
+    if (!this.app.requestSingleInstanceLock()) {
+      this.app.quit();
+    }
+
     this.app.on('ready', () => this.onReady());
     this.app.on('window-all-closed', () => this.onWindowAllClosed());
+    this.app.on('second-instance', () => this.onSecondInstance());
   }
 
   private onWindowAllClosed() {
@@ -45,6 +50,16 @@ export class Application {
 
   private onClosed() {
     this.mainWindow = null;
+  }
+
+  private onSecondInstance() {
+    if (this.mainWindow) {
+      if (this.mainWindow.isMinimized()) {
+        this.mainWindow.restore();
+      }
+
+      this.mainWindow.focus();
+    }
   }
 }
 
