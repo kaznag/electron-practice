ab24a7f44470827dfb5540cba381ee6f8de402f0
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2019-11-28 22:58:41 +0900

    Change the relative file path to file name

--- src/main/main-window.ts
+++ src/main/main-window.ts
@@ -18,7 +18,7 @@ class MainWindow {
     });
 
     this.window.setMenu(null);
-    this.window.loadFile(path.join(__dirname, './index.html'));
+    this.window.loadFile(path.join(__dirname, 'index.html'));
 
     this.window.on('close', (e: Event) => this.onClose(e));
     this.window.on('closed', () => this.onClosed());
