3e3e1e7e14b59b9c012fc93aafb3312bd85b19f1
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2020-01-15 21:18:03 +0900

    Add script to create src/package.json

---
+++ scripts/package.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootPath = path.resolve(__dirname, './../');
+const srcPath = path.resolve(rootPath, './package.json');
+const dstPath = path.resolve(rootPath, './src/package.json');
+
+const srcObj = JSON.parse(fs.readFileSync(srcPath, 'utf8'));
+const dstObj = {};
+
+Object.keys(srcObj).forEach((key) => {
+  switch (key) {
+    case 'main':
+    case 'name':
+    case 'version':
+      dstObj[key] = srcObj[key];
+      break;
+  }
+});
+
+fs.writeFileSync(dstPath, JSON.stringify(dstObj, null, '  '));
