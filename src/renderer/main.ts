764e432c46dfa6e543f1d27adcfe454193411f73
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2020-12-30 15:26:42 +0900

    Improve scss

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -3,29 +3,25 @@ import './style.scss';
 class App {
 
   private isFocused: boolean = false;
+  private isMaximized = false;
 
   private windowTitle: HTMLElement | null;
-  private maximizeButton: HTMLElement | null;
-  private restoreButton: HTMLElement | null;
+  private closeButton: HTMLElement | null;
+  private maximizeRestoreButton: HTMLElement | null;
   private minimizeButton: HTMLElement | null;
 
   constructor() {
 
     this.windowTitle = document.getElementById('window-title');
 
-    const closeButton = document.getElementById('close-button');
-    if (closeButton) {
-      closeButton.addEventListener('click', this.onCloseButtonClick);
+    this.closeButton = document.getElementById('close-button');
+    if (this.closeButton) {
+      this.closeButton.addEventListener('click', this.onCloseButtonClick);
     }
 
-    this.maximizeButton = document.getElementById('maximize-button');
-    if (this.maximizeButton) {
-      this.maximizeButton.addEventListener('click', this.onMaximizeRestoreButtonClick);
-    }
-
-    this.restoreButton = document.getElementById('restore-button');
-    if (this.restoreButton) {
-      this.restoreButton.addEventListener('click', this.onMaximizeRestoreButtonClick);
+    this.maximizeRestoreButton = document.getElementById('maximize-restore-button');
+    if (this.maximizeRestoreButton) {
+      this.maximizeRestoreButton.addEventListener('click', this.onMaximizeRestoreButtonClick);
     }
 
     this.minimizeButton = document.getElementById('minimize-button');
@@ -47,10 +43,9 @@ class App {
         }
 
         this.isFocused = windowParameter.isFocused;
-        this.updateStyles();
+        this.isMaximized = windowParameter.isMaximized;
 
-        this.displayMaximizeButton(!windowParameter.isMaximized);
-        this.displayRestoreButton(windowParameter.isMaximized);
+        this.updateStyles();
 
         window.api.sendWindowInitialized();
       });
@@ -72,21 +67,27 @@ class App {
     if (this.windowTitle) {
       this.windowTitle.className = 'window-title' + (!this.isFocused ? ' blur' : '');
     }
+
+    if (this.closeButton) {
+      this.closeButton.className = 'close-button' + (!this.isFocused ? ' blur' : '');
+    }
+
+    if (this.minimizeButton) {
+      this.minimizeButton.className = 'minimize-button' + (!this.isFocused ? ' blur' : '');
+    }
+
+    this.updateMaximizeRestoreButtonStyle();
+  }
+
+  private updateMaximizeRestoreButtonStyle(): void {
+    if (this.maximizeRestoreButton) {
+      this.maximizeRestoreButton.className = (this.isMaximized ? 'restore-button' : 'maximize-button') + (!this.isFocused ? ' blur' : '');
+    }
   }
 
   private onWindowMaximize(isMaximized: boolean): void {
-    this.displayMaximizeButton(!isMaximized);
-    this.displayRestoreButton(isMaximized);
-  }
-
-  private displayMaximizeButton(display: boolean): void {
-    const style = display ? 'block' : 'none';
-    this.maximizeButton!.style.display = style;
-  }
-
-  private displayRestoreButton(display: boolean): void {
-    const style = display ? 'block' : 'none';
-    this.restoreButton!.style.display = style;
+    this.isMaximized = isMaximized;
+    this.updateMaximizeRestoreButtonStyle();
   }
 }
 
